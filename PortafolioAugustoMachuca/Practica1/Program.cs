03d0ce39617153a523cc811eb957a55dd5df729a
AugustM99 <augusto.machuca@catolica.edu.sv>
2020-02-02 17:04:55 -0600

    practica 1 completa

--- PortafolioAugustoMachuca/Practica1/Program.cs
+++ PortafolioAugustoMachuca/Practica1/Program.cs
@@ -16,7 +16,7 @@ namespace Practica1
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form4());
+            Application.Run(new Form1());
         }
     }
 }
